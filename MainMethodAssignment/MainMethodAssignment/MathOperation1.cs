3e43c31c0b14bf55dc7db51a0dacd038587ae8fe
saifsyed0626 <saifsyed0626@gmail.com>
2026-04-14 18:16:38 -0400

    Main Method Assignment

---
+++ MainMethodAssignment/MainMethodAssignment/MathOperation1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class MathOperation1
+    {
+        //Create a method that takes two integers as parameters
+        //Make one parameter optional by providing a default value
+        public int MathOperation(int i, int j = 1)
+        {
+            //Perform multiplication on integers and return the result
+            int result = i * j;
+            return result;
+        }
+    }
+}

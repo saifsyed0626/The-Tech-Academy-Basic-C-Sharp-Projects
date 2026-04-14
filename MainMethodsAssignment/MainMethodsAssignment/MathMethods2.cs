a0fb07e9bfee92469e401d9a52247c86f9e0eea7
saifsyed0626 <saifsyed0626@gmail.com>
2026-04-14 18:03:17 -0400

    Main Methods Assignment

---
+++ MainMethodsAssignment/MainMethodsAssignment/MathMethods2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    public class MathMethods2
+    {
+        //First method that takes an integer
+        public int MathOp(int number)
+        {
+            return number + 7;
+        }
+
+        //Second method that takes a decimal
+        public int MathOp(decimal number)
+        {
+            //Convert the decimal to an int to perform math operation
+            int newnum = Convert.ToInt32(number);
+            return newnum * 20;
+        }
+
+        //Third MathOp method that takes a string
+        public int MathOp(string number)
+        {
+            //Convert the string to an int to perform math operation
+            int newnum = Convert.ToInt32(number);
+            return newnum % 3;
+        }
+    }
+}
---
+++ MainMethodsAssignment/MainMethodsAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the MathMethods2 class
+            MathMethods2 math1 = new MathMethods2();
+
+            //Call the first method with an int parameter
+            Console.WriteLine(math1.MathOp(7));
+
+            //Call the second method with a decimal parameter
+            Console.WriteLine(math1.MathOp(7.3m));
+
+            //Call the third method with a string parameter
+            Console.WriteLine(math1.MathOp("7"));
+        }
+    }
+}

f9f973f306b69e12dba1e8469d9f370fca845140
saifsyed0626 <saifsyed0626@gmail.com>
2026-04-14 18:22:56 -0400

    Method Class Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/MathClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VoidMethodAssignment
+{
+    // This class contains a void method that performs a math operation
+    public class MathClass
+    {
+        // Void method that takes two integers as parameters
+        public void DoMath(int num1, int num2)
+        {
+            // Perform a math operation on the first number
+            int result = num1 * 2;
+
+            // Display the result of the math operation
+            Console.WriteLine("First number multiplied by 2: " + result);
+
+            // Display the second number
+            Console.WriteLine("Second number is: " + num2);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create an instance of the MathClass
+            MathClass mathObj = new MathClass();
+
+            // Call the method by passing values normally
+            mathObj.DoMath(5, 10);
+
+            Console.WriteLine();
+
+            // Call the method using named parameters
+            mathObj.DoMath(num1: 7, num2: 20);
+
+            // Keep console open
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}

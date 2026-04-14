ff997641d605914162be943702fd7e94ba9028fe
saifsyed0626 <saifsyed0626@gmail.com>
2026-04-14 17:21:23 -0400

    Calling Methods Assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Ask the user for a number
+            Console.Write("Enter a number to perform math operations on: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Create an instance of the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Call each method and store the results
+            int result1 = mathOps.AddTen(userNumber);
+            int result2 = mathOps.MultiplyByTwo(userNumber);
+            int result3 = mathOps.SubtractFive(userNumber);
+
+            // Display the results to the console
+            Console.WriteLine("\nResults:");
+            Console.WriteLine("Add 10: " + result1);
+            Console.WriteLine("Multiply by 2: " + result2);
+            Console.WriteLine("Subtract 5: " + result3);
+
+            // Keep console open
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CallingMethodsAssignment/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CallingMethodsAssignment
+{
+    // This class contains methods that perform math operations
+    public class MathOperations
+    {
+        // Method 1: Adds 10 to the input number
+        public int AddTen(int number)
+        {
+            return number + 10;
+        }
+
+        // Method 2: Multiplies the input number by 2
+        public int MultiplyByTwo(int number)
+        {
+            return number * 2;
+        }
+
+        // Method 3: Subtracts 5 from the input number
+        public int SubtractFive(int number)
+        {
+            return number - 5;
+        }
+    }
+}

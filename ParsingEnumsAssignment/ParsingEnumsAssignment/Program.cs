97c6469d6f4204ba58f15184049e799bcf32fa9a
saifsyed0626 <saifsyed0626@gmail.com>
2026-04-16 09:52:39 -0400

    ParsingEnumsAssignment

---
+++ ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ParsingEnumsAssignment
+{
+    class Program
+    {
+        //Create an enum for the days of the week.
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            bool isvalid = false;
+            while (!isvalid)
+            {
+                try
+                {
+                    //Prompt the user to enter the current day of the week.
+                    Console.WriteLine("Enter the current day of the week");
+                    string dayinput = Console.ReadLine();
+
+                    //Assign the value to a variable of that enum data type you just created.
+                    DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), dayinput);
+
+                    Console.WriteLine("Have a nice " + day);
+                    Console.ReadLine();
+                    isvalid = true;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("Please enter an actual day of the week");
+                }
+            }
+        }
+    }
+}

4c4552c079860e023fa52ad9fb13a0d842a6f931
saifsyed0626 <saifsyed0626@gmail.com>
2026-04-22 16:44:44 -0400

    EntityFramework Practice Lessons

---
+++ EntityFramework/EntityFramework/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Data.Entity;
+using EntityFramework;
+using NewsletterAppMVC.Models;
+
+namespace NewsletterAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (var db = new NewsletterEntities())
+            {
+                var signups = db.SignUps.ToList();
+
+                var signupVms = signups.Select(s => new SignupVm
+                {
+                        Id = s.Id,
+                        FirstName = s.FirstName,
+                        LastName = s.LastName,
+                        EmailAddress = s.EmailAddress
+                    })
+                    .ToList();
+
+                return View(signupVms);
+            }
+        }
+    }
+}
